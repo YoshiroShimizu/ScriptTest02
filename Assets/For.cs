70b14b4e8ddd83ada976aa771e06c4bf8f99355e
yoshiroX <roboxpress@yahoo.co.jp>
2019-04-02 00:05:22 +0900

    LOSSON4基本課題修正
    メンタリングを受け、基本課題を修正したスクリプトです。

---
+++ Assets/For.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class For : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        //要素5の配列を初期化する
+        int[] array = { 100, 80, 150, 200, 190 };
+
+        //配列の要素数の分だけ処理を繰り返す
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //要素5の配列を初期化する
+        int[] points = { 100, 80, 150, 200, 190 };
+
+        //配列の要素数の分だけ処理を繰り返す
+        for (int i = points.Length-1;i >= 0; i--)
+        {
+                Debug.Log(points[i]);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
