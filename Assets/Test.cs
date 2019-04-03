0167b77bcd35fdde4ceaf3afee73eb41dab0f723
yoshiroX <roboxpress@yahoo.co.jp>
2019-04-03 23:39:38 +0900

    LESSON4発展課題のデータです。
    魔法攻撃を10回行い、11回目で足りなくなるようにしました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,20 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss{
+public class Boss
+{
     private int hp = 100;         // 体力
     private int power = 25; // 攻撃力
+    private int mp = 53; //魔法力
 
     // 攻撃用の関数
-    public void Attack(){
+    public void Attack()
+    {
         Debug.Log(this.power + "のダメージを与えた");
     }
 
     // 防御用の関数
-    public void Defence(int damage){
+    public void Defence(int damage)
+    {
         Debug.Log(damage + "のダメージを受けた");
         //残りhpを減らす
         this.hp -= damage;
+    }
+
+    // 魔法攻撃用の関数
+    public void Magic(int reducemp){
+        if (mp >= reducemp)
+        {
+            //残りmpを減らす
+            this.mp -= reducemp;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+
     }
 
 }
@@ -24,7 +43,7 @@ public class Boss{
 public class Test : MonoBehaviour{
 
 
-    void Start(){
+    void Start() {
         //Bossクラスの変数を宣言してインスタンスを代入
         Boss lastboss = new Boss();
 
@@ -32,10 +51,16 @@ public class Test : MonoBehaviour{
         lastboss.Attack();
         //防御用の関数を呼び出す
         lastboss.Defence(3);
-    }
+        //魔法攻撃用の関数を呼び出す
+        int[] mpoints = { 5, 5, 5, 5, 5, 5, 5, 5, 5,5,5};
+        for (int i = 0; i < mpoints.Length; i++) {
+            lastboss.Magic(mpoints[i]);
+        }
 
-        // Update is called once per frame
-        void Update() {
+ }
+
+    // Update is called once per frame
+    void Update(){
 
     }
 }
